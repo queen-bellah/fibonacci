3fc1ef55d7e371abd5dd2400f8620822d5acbe3e
queen-bellah <umurerwaqueenbella@gmail.com>
2024-06-17 14:07:29 +0300

    first commit

---
+++ app/src/main/java/com/akirackix/recyleviews/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.akirackix.recyleviews
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val names = listOf("Amina","Carol","Diana","Esther","Flo","Gina","helen","Innocent","Jacob","Kelvin","Lisa","Mike","Naomi","Oscar","Pearl","Queen","Robert")
+    }
+}
